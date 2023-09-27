c53b14d8aab3043cd6f2acc6f8ba14c41db7c67c
Seun Lanlege <seunlanlege@gmail.com>
2023-09-27 18:01:06 +0100

    fix L2 deployment script

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,15 @@
-source .env.prod
-# deploy
-forge script script/Deploy.s.sol:DeployScript --rpc-url goerli --broadcast --verify -vvvv --sender=$ADMIN
-# verify
-forge script script/Deploy.s.sol:DeployScript --rpc-url goerli --resume --verify -vvvv --sender=$ADMIN
+if [ "$1" = "local" ]; then
+    echo "Deploying locally"
+    # load local .env
+    source .env
+    # deploy
+    forge script script/Deploy.s.sol:DeployScript --rpc-url "$GOERLI_RPC_URL" --broadcast -vvvv --sender="$ADMIN"
+else
+    echo "Deploying to $1"
+    # load prod .env
+    source .env.prod
+    # deploy
+    forge script script/Deploy.s.sol:DeployScript --rpc-url "$1" --broadcast -vvvv --sender="$ADMIN"
+    # verify
+    forge script script/Deploy.s.sol:DeployScript --rpc-url "$1" --resume --verify -vvvv --sender="$ADMIN"
+fi
