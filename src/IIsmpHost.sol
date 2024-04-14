4d44b62b85189be568a5287b6874adc7587ae6da
Seun Lanlege <seunlanlege@gmail.com>
2024-04-14 06:22:23 +0100

    add relayer to deleteStateCommitment

--- src/IIsmpHost.sol
+++ src/IIsmpHost.sol
@@ -133,9 +133,9 @@ interface IIsmpHost is IDispatcher {
         external;
 
     /**
-     * @dev Delete the state commitment at given state height alongside relevant metadata. Assumes the state commitment is of the latest height.
+     * @dev Delete the state commitment at given state height.
      */
-    function deleteStateMachineCommitment(StateMachineHeight memory height) external;
+    function deleteStateMachineCommitment(StateMachineHeight memory height, address fisherman) external;
 
     /**
      * @dev Dispatch an incoming request to destination module
