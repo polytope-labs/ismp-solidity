19d02cd25b503c326455389a0dae767cd1ed3d04
David Salami <wizdave97@gmail.com>
2024-08-02 18:02:08 +0100

    use chain id to address evm state machines

--- interfaces/StateMachine.sol
+++ interfaces/StateMachine.sol
@@ -17,53 +17,8 @@ library StateMachine {
 		return bytes(string.concat("KUSAMA-", Strings.toString(id)));
 	}
 
-	// @notice Address the ethereum "execution layer"
-	function ethereum() internal pure returns (bytes memory) {
-		return bytes("ETH-EXEC");
-	}
-
-	// @notice Address the Polygon POS state machine
-	function polygon() internal pure returns (bytes memory) {
-		return bytes("POLY");
-	}
-
-	// @notice Address the Binance smart chain state machine
-	function bsc() internal pure returns (bytes memory) {
-		return bytes("BSC");
-	}
-
-	// @notice Address the Arbitrum state machine
-	function arbitrum() internal pure returns (bytes memory) {
-		return bytes("ETH-ARBI");
-	}
-
-	// @notice Address the Optimism state machine
-	function optimism() internal pure returns (bytes memory) {
-		return bytes("ETH-OPTI");
-	}
-
-	// @notice Address the Base state machine
-	function base() internal pure returns (bytes memory) {
-		return bytes("ETH-BASE");
-	}
-
-	// @notice Address the Blast state machine
-	function blast() internal pure returns (bytes memory) {
-		return bytes("ETH-BLST");
-	}
-
-	// @notice Address the Mantle machine
-	function mantle() internal pure returns (bytes memory) {
-		return bytes("ETH-MNTL");
-	}
-
-	// @notice Address the Manta machine
-	function manta() internal pure returns (bytes memory) {
-		return bytes("ETH-MNTA");
-	}
-
-	// @notice Address the Build on Bitcoin machine
-	function bob() internal pure returns (bytes memory) {
-		return bytes("ETH-BOB");
+	// @notice Address an evm state machine
+	function evm(uint chainid) internal pure returns (bytes memory) {
+		return bytes(string.concat("EVM-", Strings.toString(chainid)));
 	}
 }
