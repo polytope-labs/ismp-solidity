dda777ee96bccf7c8b25af50c44f20f7f2015799
Seun Lanlege <seun@polytope.technology>
2024-10-02 18:01:39 +0000

    allow consensus client return multiple intermediate states

--- interfaces/IConsensusClient.sol
+++ interfaces/IConsensusClient.sol
@@ -37,5 +37,5 @@ interface IConsensusClient {
 	function verifyConsensus(
 		bytes memory trustedState,
 		bytes memory proof
-	) external returns (bytes memory, IntermediateState memory);
+	) external returns (bytes memory, IntermediateState[] memory);
 }
